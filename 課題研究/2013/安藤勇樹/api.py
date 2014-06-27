1c13f1cc4b1cbd862dc1a9e76712d47f65452f4d
andoyuki <andou0812@gmail.com>
2014-06-27 17:20:40 +0900

    issue取得の際に使うプログラムです

---
+++ 課題研究/2013/安藤勇樹/api.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+
+import sys, json, requests
+
+
+#GitHubのログイン情報をファイルから取得する
+#TODO:パスワードに「:」を使っているとダメ
+tmp = open('github.passwd').readline().rstrip('\n').split(':');
+username = tmp[0]
+password = tmp[1]
+#print >> sys.stderr, username,password
+
+
+#APIのURLはコマンドライン引数で与える
+url = sys.argv[1]
+
+
+count = 0
+while (url is not None):
+  #print url
+  r = requests.get(url, auth=(username, password))
+  print >> sys.stderr, r.headers['status'],
+  for item in r.json():
+    count = count + 1
+    print json.dumps(item)
+  if (r.links.has_key('next')):
+    url = r.links['next']['url']
+  else:
+    url = None
+  print >> sys.stderr, count
+
